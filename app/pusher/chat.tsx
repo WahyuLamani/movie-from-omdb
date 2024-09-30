7e3af3002f9b413cd96fc95c4262fe11024c6cad
wahyulamani03 <wahyulamani03@gmail.com>
2024-09-30 02:12:25 +0000

    redeployment

--- app/pusher/chat.tsx
+++ app/pusher/chat.tsx
@@ -5,7 +5,7 @@ import Pusher from "pusher-js";
 
 export default function Chat() {
     const [messages, setMessages] = useState<string[]>([]);
-
+    // redeploy
     useEffect(() => {
         const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
             cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
